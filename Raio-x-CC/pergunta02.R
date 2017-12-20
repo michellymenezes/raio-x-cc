7051de1b2be7cfeb048fceff79d440c979c68e81
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2017-12-20 09:24:54 -0300

    Inicia visualizacao da pergunta 02

---
+++ Raio-x-CC/pergunta02.R
@@ -0,0 +1,7 @@
+
+# - Quantos alunos estão aptos a cursar a disciplina X (tem pré-requisito necessário).
+# * Gráfico de barras
+# * Possível selecionar mais de uma disciplina
+# * Cada barra é uma disciplina
+
+disciplinas.alunos_aptos = 
--- Raio-x-CC/processa_dados.R
+++ Raio-x-CC/processa_dados.R
@@ -1,14 +1,14 @@
 library(readr)
 library(dplyr)
 
-disciplinas <- read_csv("../dados/disciplinas.csv")
-matriculas <- read_csv("../dados/matriculas.csv")
-turmas <- read_csv("../dados/turmas.csv")
+disciplinas <- read_delim("dados/disciplinas.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+matriculas <- read_delim("dados/matriculas.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+turmas <- read_delim("dados/turmas.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+prerequisitos <- read_delim("dados/prerequisitos.csv", ";", escape_double = FALSE, trim_ws = TRUE) 
 
-
-# Limpando dados incorretos de matriculas
+# Limpando dados incorretos de matriculas e matricula do periodo que não acabou
 matriculas = matriculas %>%
-  filter(PERIODO_MAT >= 1999.1 & PERIODO_MAT <= 2017.2)
+  filter(PERIODO_MAT >= 1999.1 & PERIODO_MAT <= 2017.1)
 
 # Trocando NA por 0 em número de turma
 matriculas$MAT_TUR_TURMA[is.na(matriculas$MAT_TUR_TURMA)] = 0
@@ -16,3 +16,5 @@ matriculas$MAT_TUR_TURMA[is.na(matriculas$MAT_TUR_TURMA)] = 0
 matriculas.turma = matriculas %>%
   group_by(PERIODO_MAT, MAT_TUR_DIS_DISCIPLINA, MAT_TUR_TURMA) %>%
   summarise(N = n())
+
+prerequisitos = prerequisitos %>% select(-PRD_DIC_CCU_CUR_COD_CURSO, -PRD_DIC_CCU_COD_CURRICULO)
