f76d9f73e64f8a2569ff540d444e6fdc3b5d0274
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-02-09 11:54:32 -0300

    Dashboard sobre formados concluida

--- Raio-x-CC/app.R
+++ Raio-x-CC/app.R
@@ -9,7 +9,8 @@ library(plotly)
 library(viridis)
 library(viridisLite)
 
-formados = get_formados() 
+formados = get_formados()
+formados_values = list(PERIODO_EVASAO = (formados %>% select(PERIODO_EVASAO) %>% unique() %>% arrange(PERIODO_EVASAO))$PERIODO_EVASAO %>% na.omit())
 matriculas = get_matriculas()
 diciplinas = get_disciplinas()
 disciplinas_qnt_alunos_aptos = get_disciplinas_qnt_alunos_aptos()
@@ -89,20 +90,33 @@ ui <- dashboardPage(
       
       
       tabItem(tabName = "tab4",
-              h3("Quantos alunos se formaram no período X. E no total? E em um intervalo?", align = "center"),
+              h3("Quantos alunos se formaram no período X? E no total? E em um intervalo?", align = "center"),
               br(),
-              column(width = 6,
-                     box(width = NULL, highchartOutput("formados_total_stack")),
-                     box(width = NULL, uiOutput('formados_years_select'))
-       
+              fluidRow(
+                column(width = 3),
+                column(width = 6,
+                       box(width = NULL, highchartOutput("formados_pelos_periodos"))
+                ),
+                column(width = 3)
               ),
               fluidRow(
-                column(width = 6, 
-                       box(width = NULL, highchartOutput("formados_pelos_periodos"))
-                )
-              ) 
+                column(width = 2),
+                column(width = 4,
+                       box(width = NULL,highchartOutput("formados_total_stack")),
+                       box(width = NULL,
+                           uiOutput('formados_years_select'),
+                           sliderInput(inputId = "targetFormados", label = "Períodos:",
+                                       min = 0, max = length(formados_values$PERIODO_EVASAO) - 1,
+                                       step = 1, sep = "",
+                                       value = c(0, length(formados_values$PERIODO_EVASAO) - 1))
+                       )
+                ),
+                column(width = 4,
+                       box(width = NULL, highchartOutput("formados_icon_graph"))
+                ),
+                column(width = 2)
+              )
       )
-  
     )
   )
 )
@@ -114,7 +128,7 @@ server <- function(input, output, session) {
   output$selectUI <- renderUI({
     
     sel_values <- paste(paste0('"', values[["PERIODO_MAT"]], '"'), collapse = ',')
-    print(sel_values)
+    #print(sel_values)
     list(
       (HTML(
         sprintf('
@@ -191,14 +205,14 @@ server <- function(input, output, session) {
           names(temp) = c("Periodo", "Turma")
 
           mat = mat %>% rbind(temp)
-          print(tur$Turma[i])
+          #print(tur$Turma[i])
 
         }
         
-        print(mat)
+        #print(mat)
         mat  =  mat %>% left_join(n_matriculas)
         mat$n[is.na(mat$n)] = -1
-        print(mat[11:20,])
+        #print(mat[11:20,])
         
         n_matriculas$Periodo = as.numeric(n_matriculas$Periodo)
         
@@ -309,52 +323,105 @@ server <- function(input, output, session) {
   
   output$formados_total_stack <- renderHighchart({
     
-    # formados = get_formados() 
-    # formados_group = formados %>%
-    #   group_by(PERIODO_EVASAO) %>%
-    #   summarise(N = n())
-    # 
-    # cols <- viridis(33)
-    # cols <- substr(cols, 0, 7)
-    # 
-    #  hchart(formados_group, "column", hcaes(x = 0, y = N, group = PERIODO_EVASAO)) %>%
-    #   hc_yAxis(title = list(text = "Número de matrículas")) %>%
-    #   hc_xAxis(title = list(text = "Períodos"), data = NULL) %>%
-    #   hc_plotOptions( column = list(stacking = "normal"),
-    #                   series = list(marker = list(enabled = FALSE))) %>%
-    #   hc_legend(enabled = FALSE) %>%
-    #    hc_colors(cols)
+    interval_begin = (formados_values[["PERIODO_EVASAO"]][input$targetFormados + 1])[1]
+    interval_end = (formados_values[["PERIODO_EVASAO"]][input$targetFormados + 1])[2]
+    
+    formados = get_formados()
+    formados_group = formados %>%
+      group_by(PERIODO_EVASAO) %>%
+      summarise(N = n()) %>%
+      filter(
+        PERIODO_EVASAO >= (interval_begin) &
+          PERIODO_EVASAO <= (interval_end)
+      )
+    
+    cols <- viridis(33)
+    cols <- substr(cols, 0, 7)
+    
+    total_n_formados = sum(formados_group$N) 
+    annotation_y = total_n_formados + 2
+    
+    title_text = paste("Quantidade de alunos formados entre <i>", interval_begin, "</i>e<i>", interval_end, "</i>")
+    subtitle_text = paste("(total de formados desde 2001:<b>", NROW(formados), "</b>)")
+    
+    tooltip = "<b>Periodo:</b> {point.PERIODO_EVASAO}<br/><b>Qnt formados:</b> {point.y}"
+
+     hchart(formados_group, "column", hcaes(x = 0, y = N, group = PERIODO_EVASAO)) %>%
+      hc_title(text = title_text, style = list(fontSize = "14px"), align = "left") %>%
+      hc_subtitle(text = subtitle_text, style = list(fontSize = "12px"), align = "left") %>%
+      hc_yAxis(title = list(text = "Número de formados")) %>%
+      hc_xAxis(labels = list(enabled = FALSE),
+               marker = list(enabled = TRUE)) %>%
+      hc_plotOptions( column = list(stacking = "normal"),
+                      series = list(marker = list(enabled = FALSE))) %>%
+      hc_legend(enabled = FALSE) %>%
+      hc_tooltip(headerFormat = "", pointFormat = tooltip) %>%
+      hc_add_annotation(xValue = 0.3, yValue = annotation_y, 
+                        title = list(
+                          text = total_n_formados,
+                          style = list(
+                            color = "#4A148C",
+                            fontFamily = 'Tangerine',
+                            fontSize = "22"
+                          )
+                        )                        
+      ) %>%
+      hc_colors(cols)
+  
+  })
+  
+  output$formados_icon_graph <- renderHighchart({
     
     formados = get_formados()
     
     formados_group = formados %>%
+      mutate(PERIODO_EVASAO = as.integer(formados$PERIODO_EVASAO)) %>%
       group_by(PERIODO_EVASAO) %>%
       summarise(N = n()) %>%
       ungroup() %>%
-      mutate(total = sum(formados_group$N),
-             PERIODO_EVASAO = as.numeric(PERIODO_EVASAO)) 
+      mutate(total = sum(formados_group$N))
     
-    colfunc <- colorRampPalette(c("deeppink", "blue"))
+    colfunc <- colorRampPalette(c("blue4", "grey50", "deeppink", "grey60", "steelblue", 
+                                  "turquoise", "blue", "orchid", "lightpink1", "red", 
+                                  "gold4", "gold")
+                )
     colors = colfunc(NROW(formados_group))
     
     N_PERIODOS = NROW(formados_group)
     icons = rep("male", N_PERIODOS)
+    
     series = formados_group$PERIODO_EVASAO
     n = formados_group$N %>% sort(decreasing = TRUE)
-    hciconarray(series, n, icons = icons, size = 3) %>%
+    
+    title_text = paste("Quantidade de alunos formados <i>através dos anos</i>")
+
+    hciconarray(series, n, icons = icons, size = 2.5) %>%
+      hc_title(text = title_text, style = list(fontSize = "14px"), align = "left") %>%
+      hc_legend(align = "left", verticalAlign = "top", layout = "vertical", y = 30) %>%
       hc_colors(colors)
     
   })
 
   output$formados_years_select <- renderUI({
     
-    formados = mutate(PERIODO_EVASAO = as.numeric(PERIODO_EVASAO))
-    
-    sliderInput(inputId = "formados_years", label = "Anos:",
-                min = min(formados$PERIODO_EVASAO), max = max(formados$PERIODO_EVASAO), 
-                sep = "", value = c(2015.1, 2017.1)
+    m_values <- paste(paste0('"', formados_values[["PERIODO_EVASAO"]], '"'), collapse = ',')
+    list(
+      (HTML(
+        sprintf('
+                        <script type="text/javascript">
+                        $(document).ready(function() {
+                        var vals = [%s];
+                        $(\'#targetFormados\').data(\'ionRangeSlider\').update(
+                        {values:vals,
+                        min: 0,
+                        max: %s,
+                        from:[0,%s]})
+                        })
+                        </script>
+                        ', m_values,
+                length(formados_values[["PERIODO_EVASAO"]]) - 1,
+                length(formados_values[["PERIODO_EVASAO"]]) - 1)))
     )
-    
   })
   
 }
